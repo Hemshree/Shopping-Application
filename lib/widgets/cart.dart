42dab4e83b7833cf4d5b6810c0fc8e98eff90656
Hemshree <31439735+Hemshree100@users.noreply.github.com>
2019-07-08 21:22:33 +0530

    Add files via upload

---
+++ lib/widgets/cart.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatefulWidget {
+  @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text('Shopping Application'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add_shopping_cart,
+              color: Colors.white,
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
